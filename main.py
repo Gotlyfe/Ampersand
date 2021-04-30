a94aacb930f9f1464e2811f1e0b38d37e1fc7fbd
RKHarshberger <>
2021-04-30 00:50:11 +0000

    Added reactions start

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import discord
+import os
 
-client = discord.client
+client = discord.Client()
+
 
 @client.event
 async def on_ready():
-  print("We have logged in as " + client.user)
+  print("We have logged in as " + str(client.user))
 
 @client.event
 async def on_message(message):
-  if method.author = client.user:
+  if message.author == client.user:
     return
   
   if message.content.startswith("&help") or message.content.startswith("&?"):
-    await message.channel.send("Help: <empty>")
+    await message.add_reaction('😄')
+    await message.channel.send("no.")
+
+@client.event
+async def on_reaction_add(reaction, user):
+  if reaction.emoji == '😄':
+    #do Stuff
+
 
 client.run(os.getenv('TOKEN'))
