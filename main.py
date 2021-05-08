bf8e6172650313d3e0d7656c4a196d87524a42f2
RKHarshberger <>
2021-05-08 08:08:55 +0000

    Roll Function added

--- main.py
+++ main.py
@@ -1,39 +1,39 @@
 #discord
 import discord
 from discord.ext import commands
-
 #environment
 import os
-
 #random
 import random
-
 #math
 import math
-
 #time
 import time
-
-#definitions
-
-
+#regex
+import re
 
 
+#instantiations
+
+#discord client
+client = commands.Bot(command_prefix = '&')
+
+
+#initializations
+
+#random seed
+random.seed(time.time())
 
 
 #Help Function "help", "Help", "tasukete", "Tasukete", "?"
 async def Help(message):
-	await message.add_reaction('🙌')
 	helpChannel = await message.guild.create_text_channel('help' + str(random.randint(1, 8)))
 	await message.channel.send("not here.")
 	await helpChannel.send("...okay")
 	await helpChannel.send("Whats up {}?".format(message.author.mention))
+	await message.add_reaction('🙌')
 
 
-#Roll Function "roll", "Roll"
-async def Roll(message):
-	await message.add_reaction('🎲')
-	await message.channel.send(random.randint(1, 6))
 
 
 @client.event
@@ -41,42 +41,87 @@ async def on_ready():
 	print("We have logged in as " + str(client.user))
 
 @client.event
-async def on_message(message):
-	if message.author == client.user:
-		return
-	
-	if message.content.startswith("&"):
-		# message started with '&'
-
-		helpText = "help", "Help", "tasukete", "Tasukete", "?"
-		for word in helpText:
-			if message.content.startswith("&" + word):
-				await Help(message)
-
-		diceText = "roll", "Roll"
-		for word in diceText:
-			if message.content.startswith("&" + word):
-				await Roll(message)
-
-
-
-@client.event
 async def on_reaction_add(reaction, user):
-	if reaction.emoji == '😄':
-		#do Stuff
-		pass
-		
-#instantiations
+	if(user != client):
+		if reaction.emoji == '😄':
+			#do Stuff
+			helpChannel = await reaction.message.guild.create_text_channel('Random Channel' + str(random.randint(1, 47)))
+		if reaction.emoji == '😭':
+			#do Stuff
+			await reaction.message.add_reaction('🎲')
+		if reaction.emoji == '🎲':
+			#do Stuff
+			new_context = reaction.message
+			new_context.author = user
+			await roll(new_context, new_context[1:-1])
 
-#initializations
+#Ping Function
+@client.command()
+async def ping(context):
+	await context.channel.send("Pong! {:.6g}".format(client.latency * 1000) + "ms")
 
-#random seed
-random.seed(time.time())
+	await context.message.add_reaction('🏓')
 
-#discord client
-client = discord.Client()
+"""Roll Function
+@client.command()
+async def roll(context, arguments):
+	#parse the string to set variables
+	dice_type:str = ""
+	number_to_roll = 1
+	add = None
+	subtract = None
+	await context.channel.send(random.randint(1, 6))
+	await context.add_reaction('🎲')
+"""
+#mirror test command
+@client.command()
+async def test(ctx, arg):
+    await ctx.send(arg)
 
 
+@client.command()
+async def roll(ctx, *args):
+	"""
+	as &roll 2 d6
+	"""
+	print(ctx)
+	await ctx.message.add_reaction('🎲')
+	REQUIRED_NUMBER = 2
+	if (len(args) != REQUIRED_NUMBER):
+		await ctx.send("Invalid number of Arguments")
+		return False
+	try:
+		total_rolls = int(args[0])
+	except ValueError:
+		await ctx.send(f"Invalid Argument 1: {args[0]}")
+
+	if (args[1][0] == 'd'):
+		command_str = "dice"
+		dice_max = int(args[1][1:len(args[1])])
+	else:
+		await ctx.send(f"Invalid Argument 2: {args[1]}")
+		return False
+
+	args_str = f"({total_rolls},{dice_max})"
+	try:
+		rolls = eval(command_str+args_str)
+	except NameError:
+		await ctx.send("Something went wrong")
+		return False
+
+	await ctx.send(rolls)
+	await ctx.send(sum(rolls))
+
+
+
+def dice(arg1, arg2):
+	"""
+	returns tuple size(arg1) each being randint between 1 and arg2
+	"""
+	rolls = []
+	for count in range(arg1):
+		rolls.append(random.randint(1,arg2))
+	return rolls
 #main
 
 #run discord client, connect using token
